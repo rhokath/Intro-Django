fdb6b64003e4a5ed1a6557a3e49743834efe2c62
Kathryn Rhoads <kathrynrhoads@Kathryns-MBP-2.home>
2019-12-27 16:43:55 -0500

    setting up rest

--- practice/settings.py
+++ practice/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'rest_framework',
     'practice_app',
     'django.contrib.admin',
     'django.contrib.auth',
@@ -120,3 +121,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/3.0/howto/static-files/
 
 STATIC_URL = '/static/'
+
+# RESTful
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly',
+    ]
+}
